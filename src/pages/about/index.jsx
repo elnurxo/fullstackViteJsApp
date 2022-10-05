298bceed67b0fde4dbfc5642b9c862815b19e7b8
elnurxo <elnurykh@code.edu.az>
2022-10-05 10:02:27 +0400

    home and about page done

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import styles from './about.module.css'
+import aboutImg from '../../assets/about.jpeg'
 
 function About() {
   return (
-    <div>About</div>
+    <div className={styles.container}>
+        <div className={styles.box}>
+            <div className={styles.imgWrapper}>
+            <h1 className={styles.title}>About Page, nothing much in here...</h1>
+            <p style={{textAlign:'center',marginBottom:'10px'}}>Hey I'm 20 years old front-end developer</p>
+                <img className={styles.image} width={700} height={600} src={aboutImg} alt="About Page" />
+            </div>
+        </div>
+    </div>
   )
 }
 
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import styles from './home.module.css'
+import homeImg from '../../assets/homepage.jpg'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className={styles.container}>
+        <div className={styles.box}>
+            <div className={styles.imgWrapper}>
+            <h1 className={styles.title}>Home Page, nothing much in here...</h1>
+                <img className={styles.image} width={700} height={600} src={homeImg} alt="Home Page" />
+            </div>
+        </div>
+    </div>
   )
 }
 
